f4edd85b1def5e70b1334d044cbea56ca89de5ed
Hideyuki Tachibana <tachi-hi@users.noreply.github.com>
2023-03-11 00:28:58 +0900

    rtd

---
+++ docs/conf.py
@@ -0,0 +1,43 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'jamorasep'
+copyright = '2023, Hideyuki Tachibana'
+author = 'Hideyuki Tachibana'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    'autoapi.extension',
+]
+
+autodoc_default_flags = [
+   'members',
+   'private-members'
+]
+
+autodoc_mock_imports = ['jamorasep']
+
+autoapi_type = 'python'
+autoapi_dirs = ['../jamorasep']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,14 @@
+.. jamorasep documentation master file, created by
+   sphinx-quickstart on Sat Mar 11 00:25:19 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to jamorasep's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. automodule:: jamorasep
+   :members:
